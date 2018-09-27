91d990f52f387913c441a75ea816ed3859adf30b
MatAff <mathijsaffourtit@gmail.com>
2018-09-26 20:56:43 -0600

    Changes

--- up.sh
+++ up.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Update Up board kernel
 sudo add-apt-repository ppa:ubilinux/up
-#sudo apt-get autoremove --purge 'linux-.*generic'
+sudo apt update
+sudo apt -f install
+sudo apt-get autoremove --purge 'linux-.*generic'
 sudo apt-get install -y linux-image-generic-hwe-16.04-upboard
 sudo apt dist-upgrade -y
 #sudo reboot
 echo "MA: Reboot suggested"
+echo "After which use uname -a, to check if kernel update was succesful"
+
