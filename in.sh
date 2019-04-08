59848350d0289780a8d840b12baafbb10e5d0a1f
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2019-04-07 19:58:15 -0600

    Adding libi2c-dev

--- in.sh
+++ in.sh
@@ -5,6 +5,7 @@ sudo apt-get -y upgrade
 sudo apt-get install -y terminator
 sudo apt-get install -y openssh-server
 sudo apt-get install -y vim
+sudo apt-get install -y libi2c-dev
 sudo apt-get install -y i2c-tools
 
 
