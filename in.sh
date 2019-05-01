196450d7928d0539fbfa2e76df0d4aac595b1429
Mathijs Affourtit <mathijsaffourtit@gmail.com>
2019-05-01 10:06:31 -0600

    Add more

--- in.sh
+++ in.sh
@@ -7,5 +7,10 @@ sudo apt-get install -y openssh-server
 sudo apt-get install -y vim
 sudo apt-get install -y libi2c-dev
 sudo apt-get install -y i2c-tools
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y make
+sudo apt-get install -y make-guile
+sudo apt-get install -y cmake
+sudo apt-get install -y xfce4 xfce4-goodies tightvncserver
 
 
