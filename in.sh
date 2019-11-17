3babee74d2c7a52cff450b78af38c8706eeb096d
MatAff <mathijsaffourtit@gmail.com>
2019-11-17 14:58:02 -0700

    adding notepad plus plus

--- in.sh
+++ in.sh
@@ -13,8 +13,10 @@ sudo apt-get install -y make-guile
 sudo apt-get install -y cmake
 sudo apt-get install -y xfce4 xfce4-goodies tightvncserver
 sudo apt-get install -y spyder3
-sudo apt-get install -y python 3.8
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
+sudo apt-get install -y python 3.8
+sudo apt-get install -y snapd snapd-xdg-open
+sudo snap install -y notepad-plus-plus
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
